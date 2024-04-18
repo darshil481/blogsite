6a92c1617138d3fc3564ae6d76e020f96c9b6cb3
DARSHIL <darshil.parmar.23.esparkbiz@gmail.com>
2024-04-19 00:29:12 +0530

    console remove

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 import App from './app';
-// const app = new App([new AuthRoute()]);
 const app = new App();
 app.listen();
--- src/api/Controller/auth.controller.ts
+++ src/api/Controller/auth.controller.ts
@@ -10,15 +10,11 @@ export class AuthController{
       this.authService = new AuthService();
     }
     public register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-      console.log("=================>")
       const { first_name, last_name, user_name, email, password, mob_no } = req.body;
-      console.log(first_name, last_name, user_name, email, password, mob_no)
       const user = await this.authService.getUser(user_name);
-
       if (user) {
         return generalResponse(res, null, 'User is already registered with this email or username!', 'error', true, 500);
       }
-
       //encrypt password
       const saltRounds = 10;
       const hashedPassword = await bcrypt.hash(password, saltRounds);
